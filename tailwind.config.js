bd5132d3eed3464cea7d3de1eea3f4c9bcc60c6e
AleksBar <nesferat@mail.ru>
2026-06-03 22:13:17 +0300

    config(tailwind): Настройки шрифта и цветов

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     './src/**/*.{vue,js,ts}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Arial', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        ['page-bg']: '#FBFBFB',
+        accent: '#0029FF',
+        gray: '#DCDCDC',
+      },
+    },
   },
   plugins: [],
 }
