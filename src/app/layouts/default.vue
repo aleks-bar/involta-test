fde73c6c14b2673c1514dc3861c2186ffc691727
AleksBar <nesferat@mail.ru>
2026-06-04 16:31:48 +0300

    refactor(layout): Добавлена верстка + фича обновления новостей

--- src/app/layouts/default.vue
+++ src/app/layouts/default.vue
@@ -1,13 +1,25 @@
 <script setup lang="ts">
+import { ArticlesRefresh } from '@features/articles-refresh'
+
 const route = useRoute()
 </script>
 
 <template>
   <header>
-    <div>{{ route.meta.h1 ?? 'Главная' }}</div>
+    <div class="container">
+      <div class="border-b py-9 border-gray-border">
+        <div class="flex items-center gap-7">
+          <h1 class="text-4xl font-bold">
+            {{ route.meta.h1 ?? 'Главная' }}
+          </h1>
+
+          <articles-refresh />
+        </div>
+      </div>
+    </div>
   </header>
 
-  <main class="bg-page-bg">
+  <main>
     <slot />
   </main>
 </template>
