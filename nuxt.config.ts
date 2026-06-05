70215784f6657c1b983e0075d4d1af3a6281879e
AleksBar <nesferat@mail.ru>
2026-06-05 09:56:28 +0300

    config: Добавлена pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 
-  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', 'nuxt-svgo'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+    'nuxt-svgo',
+  ],
   devtools: { enabled: true },
 
   dir: {
