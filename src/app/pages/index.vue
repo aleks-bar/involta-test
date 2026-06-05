00b154fec601b398cd5102b353b9478c0205ef7d
AleksBar <nesferat@mail.ru>
2026-06-05 13:33:07 +0300

    refactor: На главную добавлена логика обработки параметров из url

--- src/app/pages/index.vue
+++ src/app/pages/index.vue
@@ -1,18 +1,38 @@
 <script setup lang="ts">
-import { type Article, ArticleCard, type ArticleSourceType, ArticlesSources, useArticlesStore } from '@entities/article'
+import {
+  type Article,
+  ArticleCard,
+  type ArticleSourceType,
+  ArticlesSources,
+  getArticlesByPage,
+  predicateByArticleSearch,
+  useArticlesStore,
+} from '@entities/article'
+
+import { Pagination } from '@shared/ui/Pagination'
 
 definePageMeta({
   h1: 'Список новостей',
 })
 
+const articlesStore = useArticlesStore()
 const route = useRoute()
 const activeSource = computed<string | undefined>(() => route.query.source ? route.query.source as string : undefined)
+const search = computed<string | undefined>(
+  () => route.query.search ? decodeURIComponent(route.query.search as string) : undefined,
+)
+const currentPage = computed<number>(() => {
+  if (!route.query.page) return 1
+  const page = +route.query.page
 
-const articlesStore = useArticlesStore()
+  if (Number.isNaN(page) || page < 1) return 1
+  return page
+})
 
 const { data } = await useAsyncData('articles-data', () => articlesStore.getArticles())
 
-const articles = computed<Article[]>(() => {
+/** Получение списка Article относительно выбранного ресурса */
+const articlesList = computed<Article[]>(() => {
   if (!data.value) return []
 
   if (activeSource.value && data.value.response && activeSource.value in data.value.response) {
@@ -24,23 +44,50 @@ const articles = computed<Article[]>(() => {
 })
 
 const sources = computed(() => data.value?.sources ?? [])
+
+/** Отфилтрованные по поиску новости */
+const articlesBySearch = computed(() => {
+  return !search.value
+    ? articlesList.value
+    : articlesList.value.filter(article => predicateByArticleSearch(article, search.value as string))
+})
+
+const articles = computed(() => getArticlesByPage(articlesBySearch.value, currentPage.value, articlesStore.postsPerPage))
 </script>
 
 <template>
   <div class="container">
-    <div class="py-6 pb-7">
+    <div class="py-6 pb-7 flex items-center justify-between">
       <articles-sources
         :active-source="activeSource"
         :sources="sources"
       />
+      <div>display changer</div>
     </div>
 
-    <div class="grid grid-cols-2 gap-5">
-      <article-card
-        v-for="(article, index) in articles.slice(0, 4)"
-        :key="index"
-        v-bind="article"
+    <template v-if="articles.length">
+      <div class="grid grid-cols-2 gap-5">
+        <article-card
+          v-for="(article, index) in articles"
+          :key="index"
+          v-bind="article"
+        />
+      </div>
+
+      <pagination
+        class="mt-12"
+        :current-page="currentPage"
+        :total-posts="articlesBySearch.length"
+        :disabled="articlesStore.loading"
+        :posts-per-page="articlesStore.postsPerPage"
       />
+    </template>
+
+    <div
+      v-else
+      class="py-20 flex justify-center"
+    >
+      <span>Новости отсутствуют</span>
     </div>
   </div>
 </template>
