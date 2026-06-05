10bea0e94d66d6f66de5a8f1f568b8a5288394a2
AleksBar <nesferat@mail.ru>
2026-06-05 10:22:49 +0300

    feat: Добавлен стор для articles

---
+++ src/entities/article/model/useArticlesStore.ts
@@ -0,0 +1,29 @@
+import type { StoreArticlesResponse } from '@entities/article/model/StoreArticlesResponse'
+import { articlesResponseToStoreResponse } from '@entities/article/utils/responseAdapter'
+import { getRss } from '~/server/utils/getRss'
+
+export const useArticlesStore = defineStore('articles', () => {
+  const { $api } = useNuxtApp()
+  const _loading = ref<boolean>(false)
+
+  const getArticles = async (): Promise<StoreArticlesResponse | null> => {
+    _loading.value = true
+
+    try {
+      const data = import.meta.client ? await $api.articles.getArticles() : await getRss()
+      return articlesResponseToStoreResponse(data)
+    }
+    catch {
+      console.error('Failed to fetch articles')
+      return null
+    }
+    finally {
+      _loading.value = false
+    }
+  }
+
+  return {
+    loading: computed(() => _loading.value),
+    getArticles,
+  }
+})
