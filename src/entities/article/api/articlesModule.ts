302430f120bd49326fba406101050b42d9532146
AleksBar <nesferat@mail.ru>
2026-06-05 10:12:10 +0300

    refactor(api): Добавлен тип ответа в api модуль

--- src/entities/article/api/articlesModule.ts
+++ src/entities/article/api/articlesModule.ts
@@ -1,7 +1,10 @@
 import HttpFactory from '@shared/api/factory'
+import type { ArticleSourceType, SourceWithArticles } from '@entities/article'
+
+export type GetArticlesResponse = Record<ArticleSourceType, SourceWithArticles>
 
 export class ArticlesModule extends HttpFactory {
   async getArticles() {
-    return await this.get('/articles')
+    return await this.get<GetArticlesResponse>('/articles')
   }
 }
