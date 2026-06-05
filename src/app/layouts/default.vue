4fb720aff75a549b4b5ad3018379c163e2ca770b
AleksBar <nesferat@mail.ru>
2026-06-05 16:44:46 +0300

    refactor(layout): Адаптив для хедера

--- src/app/layouts/default.vue
+++ src/app/layouts/default.vue
@@ -10,21 +10,26 @@ const articlesStore = useArticlesStore()
 <template>
   <header>
     <div class="container">
-      <div class="border-b py-9 border-gray-border flex items-center justify-between">
-        <div class="flex items-center gap-7">
-          <h1 class="text-4xl font-bold">
+      <div
+        class="border-b py-6 md:py-9 border-gray-border flex flex-col md:flex-row gap-5 items-center justify-between"
+      >
+        <div class="flex items-center justify-between md:justify-start gap-7 w-full md:w-auto">
+          <h1 class="text-2xl md:text-4xl font-bold">
             {{ route.meta.h1 ?? 'Главная' }}
           </h1>
 
           <articles-refresh :disabled="articlesStore.loading" />
         </div>
 
-        <articles-search :disabled="articlesStore.loading" />
+        <articles-search
+          class="w-full md:max-w-[320px]"
+          :disabled="articlesStore.loading"
+        />
       </div>
     </div>
   </header>
 
-  <main>
+  <main class="pb-20">
     <slot />
   </main>
 </template>
