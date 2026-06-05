0c573832267335b606b2ba8d85f179456701108c
AleksBar <nesferat@mail.ru>
2026-06-05 10:15:55 +0300

    refactor(article): Добавлены экспорты в паблик апи для article

--- src/entities/article/index.ts
+++ src/entities/article/index.ts
@@ -1,7 +1,21 @@
 import ArticleCard from './ui/ArticleCard.vue'
+import ArticlesSources from './ui/ArticlesSources.vue'
+import { useArticlesStore } from './model/useArticlesStore'
+import { lentaRssItemToArticle, mosRssItemToArticle } from './utils/adapters'
+
+export { ARTICLES_SOURCES, LENTA_URL, MOS_URL } from './const/articlesSources'
+
+export { ArticlesModule, type GetArticlesResponse } from './api/articlesModule'
 
 export {
   ArticleCard,
+  ArticlesSources,
+  useArticlesStore,
+  mosRssItemToArticle,
+  lentaRssItemToArticle,
 }
 
 export type { Article } from './model/Article'
+export type { ArticleSourceType, SourceWithArticles } from './model/ArticlesSource'
+export * from './model/Rss'
+export { comparatorByArticleDateDESC } from './utils/sort'
