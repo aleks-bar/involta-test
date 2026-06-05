1a061c0682632a0e2aec7d345aa41614d77d22ab
AleksBar <nesferat@mail.ru>
2026-06-05 09:57:41 +0300

    feat(utils): Добавлена утилита для работы с датами

---
+++ src/shared/utils/date.ts
@@ -0,0 +1,13 @@
+export function dateISOtoShort(isoDate: string) {
+  const date = new Date(isoDate)
+
+  if (isNaN(date.getTime())) {
+    return isoDate
+  }
+
+  const day = String(date.getDate()).padStart(2, '0')
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const year = date.getFullYear()
+
+  return `${day}.${month}.${year}`
+}
